4281c505666852662d7e624ae923a91622480069
Liam <liam@scalzulli.com>
2023-05-12 15:09:48 -0400

    Don't derive parser on store

--- src/store.rs
+++ src/store.rs
@@ -1,23 +1,11 @@
 use super::*;
 
-#[derive(Debug, Parser)]
+#[derive(Debug)]
 pub(crate) struct Store {
   path: PathBuf,
 }
 
 impl Store {
-  /// Returns a list of entries in the store.
-  pub(crate) fn entries(&self) -> Result<Vec<Entry>> {
-    Ok(
-      WalkDir::new(&self.path)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| e.file_type().is_file())
-        .map(|e| Entry::try_from(e.into_path()))
-        .collect::<Result<Vec<Entry>>>()?,
-    )
-  }
-
   /// Load or create the store from the default location.
   ///
   /// The default location is `~/.skel`.
@@ -30,9 +18,21 @@ impl Store {
     })
   }
 
+  /// Returns a list of entries in the store.
+  pub(crate) fn entries(&self) -> Result<Vec<Entry>> {
+    WalkDir::new(&self.path)
+      .into_iter()
+      .filter_map(Result::ok)
+      .filter(|e| e.file_type().is_file())
+      .map(|e| Entry::try_from(e.into_path()))
+      .collect::<Result<Vec<Entry>>>()
+  }
+
   /// Write a new entry to the store.
+  ///
+  /// The entry will be written to `~/.skel/{name}.skel`.
   pub(crate) fn write(&self, name: &str, content: &str) -> Result {
-    fs::write(self.path.join(format!("{name}.skel")), content)?;
-    Ok(())
+    fs::write(self.path.join(format!("{name}.skel")), content)
+      .map_err(|e| e.into())
   }
 }
